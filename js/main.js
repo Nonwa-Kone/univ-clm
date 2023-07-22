6dc104ae150e2f0f15ef1f18f5a448b5db86676a
Nonwa-Kone <konenonwa1998@gmail.com>
2023-07-22 22:13:53 +0200

    Nouvelle modification

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@
 
     // Sticky Navbar
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
+        if ($(this).scrollTop() > 100) {
             $('.sticky-top').addClass('shadow-sm').css('top', '0px');
         } else {
             $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
